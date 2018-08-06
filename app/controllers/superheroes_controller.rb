c7d27b339225886afe27527cf201997fac06b1fe
anfperez <anfperez@gmail.com>
2018-08-05 23:54:10 -0400

    started on new superhero page

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -3,11 +3,24 @@ class SuperheroesController < ApplicationController
     @superheroes = Superhero.all
   end
 
+  def show
+    @superhero = Superhero.find(params[:name])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:superhero].inspect
+    # render plain: params[:superhero].inspect
+    @superhero = Superhero.new(superhero_params)
+
+    @superhero.save
+
+    redirect_to @superhero
+  end
+
+  private def superhero_params
+    params.require(:superhero).permit(:name, :super_name)
   end
 
 end
